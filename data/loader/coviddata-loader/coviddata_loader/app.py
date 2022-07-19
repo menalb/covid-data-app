656cc8820d5005e028c2371b031460aa98759a8c
Alberto Meneghini <menalb84@gmail.com>
2022-07-19 19:25:44 +0200

    schedule

--- data/loader/coviddata-loader/coviddata_loader/app.py
+++ data/loader/coviddata-loader/coviddata_loader/app.py
@@ -1,9 +1,6 @@
-# import json
-import requests
-import os
 import boto3
-from botocore.exceptions import ClientError
-from utils import downloadFile
+import os
+from utils import downloadFile, uploadFile
 from data_prep.region import buildRegionDataSet
 
 DownloadFileName = os.environ.get('DOWNLOAD_FILE_NAME')
@@ -21,7 +18,8 @@ def lambda_handler(event, context):
 
         buildRegionDataSet(downloadFilePath,destinationFilePath)
 
-        uploadFile(destinationFilePath+'.parquet',BucketName,'test/'+RegionFileName)
+        uploadFile(s3, destinationFilePath+'.parquet',
+                   BucketName, RegionFileName+'.parquet')
 
 
     except Exception as e:
@@ -29,26 +27,3 @@ def lambda_handler(event, context):
         raise e
 
     return {}
-
-
-def uploadFile(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        print(e)
-        return False
-    return True
--- data/loader/coviddata-loader/coviddata_loader/utils.py
+++ data/loader/coviddata-loader/coviddata_loader/utils.py
@@ -1,7 +1,34 @@
 from fileinput import filename
+from botocore.exceptions import ClientError
+import boto3
+import os
 import requests
+
 
 def downloadFile(url:str,destinationFileName:str):
     r = requests.get(url)
     with open(destinationFileName,'wb') as f:
         f.write(r.text.encode('utf8'))
+
+
+def uploadFile(s3_client, file_name, bucket, object_name=None):
+    """Upload a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    if object_name is None:
+        object_name = os.path.basename(file_name)
+
+    # Upload the file
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, bucket, object_name)
+    except ClientError as e:
+        print(e)
+        return False
+    return True
